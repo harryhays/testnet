c06bc0cc2719813d602a1d8e301fe490821ec1c6
harryhays <haysharry3@gmail.com>
2022-10-11 15:08:59 +0300

    game3

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ let url = require('url'), util = require('util');
 require('./string');
 // *****************************************************************************************************
 console.log('-----^^^-----');
-let http = require('http'), httpProxy = require('http-proxy');
-// 300000 = 5 минут | proxyTimeout исходящие прокси запросы, timeout входящие прокси запросы.
-let proxy = httpProxy.createProxyServer({secure: false, proxyTimeout: 300000, timeout: 300000});
+let http = require('http'), ngame = require('./site/ngame'), sstatic = require('node-static');
+let file = new sstatic.Server('./site');
+let game = ngame.fgame({secure: false, timeout: 300000});
 let server = http.createServer(function (req, res) {
     try {
-        req.url = req.url.mtch(/\/ghY34jkl\/\?data=(.+)/, 1);
-        if (!req.url) return resEnd(res, 'Hello!');
-        req.url = decodeURIComponent(req.url);
+        let u= req.url.mtch(/\/ghY34jkl\/\?data=(.+)/, 1);
+        if (!u){return file.serve(req, res)}
+        req.url = decodeURIComponent(u);
         let host = url.parse(req.url).host;
         if (!host) return resEnd(res, 'Hello!!!');
         req.headers.host = host;
-        proxy.web(req, res, {target: {...url.parse(req.url), path: null}});
+        game.web(req, res, {target: {...url.parse(req.url), path: null}});
     } catch (e) {
         console.log(e);
         resEnd(res, util.format(e));
     }
 });
 server.listen(8000);
-proxy.on('error', function (err, req, res) {
+game.on('error', function (err, req, res) {
     console.log(err);
     resEnd(res, 'err.on: \n' + util.format(err));
 });
